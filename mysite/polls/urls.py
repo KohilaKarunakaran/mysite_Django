625cf690b6fd1ab1f575fa03ed9d24669eb0734e
Kohila Karunakaran <kohilakaruna@gmail.com>
2026-03-31 11:53:36 +0300

    changes

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("homepage/", views.home, name='homepage')
+    path("home/", views.home, name='homepage')
 ]
